111d8f8afc24a0d7f2fcae319ee415580de5d0da
Adam Stelmaszczyk <stelmaszczykadam@gmail.com>
2015-01-04 19:48:41 +0100

    Auto code format, changed context.ALARM_SERVICE to Context.ALARM_SERVICE (removed warning), fixed crashing after options button click (removed everything connected to options - we are not using them)

--- app/src/main/java/coclauncher/pl/coclauncher/MainActivity.java
+++ app/src/main/java/coclauncher/pl/coclauncher/MainActivity.java
@@ -10,105 +10,73 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+public class MainActivity extends ActionBarActivity {
+    private Button startBtn;
+    private TextView descriptionView;
 
-public class MainActivity extends ActionBarActivity
-{
-	private Button startBtn;
-	private TextView descriptionView;
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
-	@Override
-	protected void onDestroy()
-	{
-		super.onDestroy();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button startNowBtn = (Button) findViewById(R.id.start_now_btn);
+        startBtn = (Button) findViewById(R.id.start_btn);
+        descriptionView = (TextView) findViewById(R.id.description_view);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Button startNowBtn = (Button) findViewById(R.id.start_now_btn);
-		startBtn = (Button) findViewById(R.id.start_btn);
-		descriptionView = (TextView) findViewById(R.id.description_view);
+        if (isLauncherActive()) {
+            startBtn.setBackgroundResource(R.drawable.on);
+            descriptionView.setText(R.string.shield_active);
+        } else {
+            startBtn.setBackgroundResource(R.drawable.off);
+            descriptionView.setText(R.string.shield_not_active);
+        }
 
-		if (isLauncherActive()) {
-			startBtn.setBackgroundResource(R.drawable.on);
-			descriptionView.setText(R.string.shield_active);
-		}else{
-			startBtn.setBackgroundResource(R.drawable.off);
-			descriptionView.setText(R.string.shield_not_active);
-		}
+        startNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.supercell.clashofclans");
+                startActivity(launchIntent);
+            }
+        });
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isLauncherActive()) {
+                    LauncherService.cancelChecker(MainActivity.this);
+                    startBtn.setBackgroundResource(R.drawable.off);
+                    descriptionView.setText(R.string.shield_not_active);
+                    setPrefLauncherActive(false);
 
-		startNowBtn.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-				Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.supercell.clashofclans");
-				startActivity(launchIntent);
-			}
-		});
-		startBtn.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-				if (isLauncherActive()) {
-					LauncherService.cancelChecker(MainActivity.this);
-					startBtn.setBackgroundResource(R.drawable.off);
-					descriptionView.setText(R.string.shield_not_active);
-					setPrefLauncherActive(false);
+                } else {
+                    LauncherService.startLauncher(MainActivity.this, Settings.DEFAULT_FREQUENCY);
+                    startBtn.setBackgroundResource(R.drawable.on);
+                    descriptionView.setText(R.string.shield_active);
+                    setPrefLauncherActive(true);
+                }
+            }
+        });
+    }
 
-				} else {
-					LauncherService.startLauncher(MainActivity.this, Settings.DEFAULT_FREQUENCY);
-					startBtn.setBackgroundResource(R.drawable.on);
-					descriptionView.setText(R.string.shield_active);
-					setPrefLauncherActive(true);
-				}
-			}
-		});
-	}
+    private boolean isLauncherActive() {
+        boolean launcherActive = LauncherService.isLauncherIntentActive(MainActivity.this);
+        boolean launcherActiveFromPrefs = getPrefLauncherActive();
+        return launcherActive && launcherActiveFromPrefs;
+    }
 
-	private boolean isLauncherActive()
-	{
-		boolean launcherActive = LauncherService.isLauncherIntentActive(MainActivity.this);
-		boolean launcherActiveFromPrefs = getPrefLauncherActive();
-		return launcherActive && launcherActiveFromPrefs;
-	}
+    public void setPrefLauncherActive(boolean active) {
+        SharedPreferences settings = getSharedPreferences(Settings.PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean(Settings.PREF_LAUNCHER_ACTIVE, active);
+        editor.commit();
+    }
 
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		int id = item.getItemId();
-		switch (id) {
-		case R.id.action_settings:
-			return true;
-		case R.id.action_test:
-			//			LauncherService.startLauncher(this, 15 * 1000);
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	public void setPrefLauncherActive(boolean active)
-	{
-		SharedPreferences settings = getSharedPreferences(Settings.PREFS_NAME, 0);
-		SharedPreferences.Editor editor = settings.edit();
-		editor.putBoolean(Settings.PREF_LAUNCHER_ACTIVE, active);
-		editor.commit();
-	}
-
-	public boolean getPrefLauncherActive()
-	{
-		SharedPreferences settings = getSharedPreferences(Settings.PREFS_NAME, 0);
-		return settings.getBoolean(Settings.PREF_LAUNCHER_ACTIVE, false);
-	}
+    public boolean getPrefLauncherActive() {
+        SharedPreferences settings = getSharedPreferences(Settings.PREFS_NAME, 0);
+        return settings.getBoolean(Settings.PREF_LAUNCHER_ACTIVE, false);
+    }
 
 }
