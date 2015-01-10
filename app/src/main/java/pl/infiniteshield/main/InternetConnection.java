fae0c57a954b0bdeeae59d35a1bd3c531193670e
Adam Stelmaszczyk <stelmaszczykadam@gmail.com>
2015-01-10 17:33:02 +0100

    InternetConnection class and 2 permissions

---
+++ app/src/main/java/pl/infiniteshield/main/InternetConnection.java
@@ -0,0 +1,36 @@
+package pl.infiniteshield.main;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+
+public class InternetConnection {
+
+    public static void setEnabled(Context context, boolean enabled) {
+        if (isWifi()) {
+            setWifiEnabled(context, enabled);
+        }
+        if (isNetwork()) {
+            setNetworkEnabled(context, enabled);
+        }
+    }
+
+    private static boolean isWifi() {
+        // TODO: implement
+        return false;
+    }
+
+    private static boolean isNetwork() {
+        // TODO: implement
+        return false;
+    }
+
+    private static void setNetworkEnabled(Context context, boolean enabled) {
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        wifiManager.setWifiEnabled(enabled);
+    }
+
+    private static void setWifiEnabled(Context context, boolean enabled) {
+        // TODO: implement
+    }
+
+}
