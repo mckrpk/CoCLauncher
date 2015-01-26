9e27a66630b078fb72c836e3c99a06d1d2f7b534
Michał Karpiuk <karp157@gmail.com>
2015-01-27 00:59:08 +0100

    Help dialog.

--- app/src/main/java/com/infiniteshield/main/MainActivity.java
+++ app/src/main/java/com/infiniteshield/main/MainActivity.java
@@ -1,11 +1,15 @@
 package com.infiniteshield.main;
 
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -41,6 +45,7 @@ public class MainActivity extends ActionBarActivity {
         registerReceiver(shieldStateChangedReceiver, new IntentFilter(SHIELD_STATE_CHANGED_ACTION));
     }
 
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -61,6 +66,32 @@ public class MainActivity extends ActionBarActivity {
             startShield.setBackgroundResource(R.drawable.off);
             shieldStatus.setText(R.string.shield_not_active);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.help) {
+            showHelpDialog();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showHelpDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        TextView contentView = new TextView(this);
+        contentView.setTextColor(Color.BLACK);
+        contentView.setPadding(10, 10, 10, 10);
+        contentView.setText(getString(R.string.help_content));
+        builder.setTitle(R.string.help_title).setView(contentView);
+
+        builder.create().show();
     }
 
 }
